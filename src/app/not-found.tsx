06141ad5854efc1cefff7542b271187de4705cff
rae <rae@raeveira.nl>
2026-02-27 10:51:41 +0100

    updated the not-found page with the correct import

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { ArrowLeft, Home } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import NotFoundImage from "@/../public/not-found-image.png";
+import NotFoundImage from "@/../public/images/not-found-image.png";
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {

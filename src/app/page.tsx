0a452ba86d7a1ab2d701aa239fa492803b8c3248
rae <rae@raeveira.nl>
2026-03-03 09:27:26 +0100

    added a info screen to the game on initial launch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Settings } from "lucide-react";
+import { InfoScreen } from "@/components/info-screen";
 import { Button } from "@/components/ui/button";
 import { GameOverScreen } from "@/components/wordle/game-over-screen";
 import { WordleGrid } from "@/components/wordle/wordle-grid";
@@ -19,19 +20,22 @@ const Home = () => {
   } = useWordle();
 
   return (
-    <div className="flex flex-col items-center gap-4 p-4">
-      <div className="w-full max-w-lg flex justify-end">
-        <Button variant="outline" size="icon" className="rounded-xl">
-          <Settings className="w-4 h-4" />
-        </Button>
-      </div>
-      <WordleGrid guesses={guesses} currentGuess={currentGuess} turn={turn} />
-      <WordleKeyboard letterStatus={letterStatus} onKey={handleKey} />
+    <>
+      <InfoScreen />
+      <div className="flex flex-col items-center gap-4 p-4">
+        <div className="w-full max-w-lg flex justify-end">
+          <Button variant="outline" size="icon" className="rounded-xl">
+            <Settings className="w-4 h-4" />
+          </Button>
+        </div>
+        <WordleGrid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+        <WordleKeyboard letterStatus={letterStatus} onKey={handleKey} />
 
-      {gameOver && (
-        <GameOverScreen targetWord={targetWord} resetGame={resetGame} />
-      )}
-    </div>
+        {gameOver && (
+          <GameOverScreen targetWord={targetWord} resetGame={resetGame} />
+        )}
+      </div>
+    </>
   );
 };
 
